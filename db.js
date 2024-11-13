3109f67d8ae17ef95eee0f0b2a13ac65a6906bf6
Bhabani <bhabaniswain2002@gmail.com>
2024-11-13 11:52:01 +0530

    Ignore node_modules folder

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const mongoURL = process.env.MONGODB_URL_LOCAL;
 
-mongoose.connect(mongoURL)
+mongoose
+  .connect(mongoURL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("MongoDB connection error:", error));
 const db = mongoose.connection;
